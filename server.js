b708b51ceef491ed129af24e8a3bc47b525bf121
filipclaesson <filipclaesson@hotmail.com>
2017-04-29 21:34:56 +0200

    change source for images

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/takePicture', function (req, res){
 	var times = getTimestamp(time);
 	var timestamp = times.timestamp;	
 	// ---- Skapa ny cameramodul -------
-	var photo_location = "/home/pi/Desktop/filip/webcam/public/images/bild-" + timestamp + ".jpg";
+	var photo_location = "/home/pi/node_apps/ruttnero_services/public/images/bild-" + timestamp + ".jpg";
 
 	var camera = new RaspiCam({
 		mode: "photo", 
